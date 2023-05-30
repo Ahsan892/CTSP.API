9f7ec1a92cbb036f9be87e5c4eda33f2aa241166
SMOKEYSEKS <SMOKEYSEKS@SMOKEYSEKS>
2023-05-30 22:12:24 +0500

    Add project files.

---
+++ CTSP.API/Controllers/CTSPController.cs
@@ -0,0 +1,147 @@
+﻿using CTSP.API.Data;
+using CTSP.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Reflection.Emit;
+
+namespace CTSP.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CTSPController : ControllerBase
+    {
+        private readonly CTSPRecruiterContext _context;
+
+        public CTSPController(CTSPRecruiterContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Country")]
+        public async Task<IActionResult> SPR_CountryAsync(byte? opcode, short? CountryID, string? Country)
+        {
+            try
+            {
+                var result = await _context.GetProcedures().SPR_CountryAsync(opcode, CountryID, Country);
+                return Ok(result);
+            }
+            catch (SqlException ex) when (ex.Message.Contains("Error:"))
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        [HttpGet("Province")]
+        public async Task<IActionResult> SPR_ProvinceAsync(byte? opcode, short? ProvinceID, string? Province, short? CountryID, short? Search_CID)
+        {
+            try
+            {
+                var result = await _context.GetProcedures().SPR_ProvinceAsync(opcode, ProvinceID, Province, CountryID,Search_CID);
+                return Ok(result);
+            }
+            catch (SqlException ex) when (ex.Message.Contains("Error:"))
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("GetProvince")]
+        public async Task<IActionResult> SPR_DropDownsAsync(byte? opcode, long? UserID, string WhereID, long? PostsID)
+        {
+            try
+            {
+                var result = await _context.GetProcedures().SPR_DropDownsAsync(opcode, UserID,WhereID,PostsID);
+                return Ok(result);
+            }
+            catch (SqlException ex) when (ex.Message.Contains("Error:"))
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        } 
+        
+        [HttpGet("GetDivision")]
+        public async Task<IActionResult> SPR_DivisionAsync(byte? opcode, short? DivisionID, string? Division, short? ProvinceID, short? Search_ProvinceID)
+        {
+            try
+            {
+                var result = await _context.GetProcedures().SPR_DivisionAsync(opcode,DivisionID,Division,ProvinceID,Search_ProvinceID);
+                return Ok(result);
+            }
+            catch (SqlException ex) when (ex.Message.Contains("Error:"))
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet("GetDistrict")]
+        public async Task<IActionResult> SPR_DistrictAsync(byte? Opcode, short? DistrictID, string? District, short? DivisionID, short? Search_DivID)
+        {
+            try
+            {
+                var result = await _context.GetProcedures().SPR_DistrictAsync(Opcode,DistrictID,District,DivisionID,Search_DivID);
+                return Ok(result);
+            }
+            catch (SqlException ex) when (ex.Message.Contains("Error:"))
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        
+        [HttpGet("GetTehsil")]
+        public async Task<IActionResult> SPR_TehsilAsync(byte? opcode, short? TehsilID, string? Tehsil, short? DistrictID, short? Search_DistID)
+        {
+            try
+            {
+                var result = await _context.GetProcedures().SPR_TehsilAsync(opcode,TehsilID,Tehsil,DistrictID, Search_DistID);
+                return Ok(result);
+            }
+            catch (SqlException ex) when (ex.Message.Contains("Error:"))
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        } 
+        [HttpGet("GetUC")]
+        public async Task<IActionResult> SPR_UCAsync(byte? opcode, short? UCID, string? UCName, short? TehsilID, short? Search_TehsilID)
+        {
+            try
+            {
+                var result = await _context.GetProcedures().SPR_UCAsync(opcode,UCID,UCName,TehsilID,Search_TehsilID);
+                return Ok(result);
+            }
+            catch (SqlException ex) when (ex.Message.Contains("Error:"))
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+    }
+}
---
+++ CTSP.API/Program.cs
@@ -0,0 +1,40 @@
+using CTSP.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var devCorsPolicy = "devCorsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(devCorsPolicy, builder => {
+        //builder.WithOrigins("http://localhost:800").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        //builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
+        //builder.SetIsOriginAllowed(origin => true);
+    });
+});
+builder.Services.AddDbContext<CTSPRecruiterContext>(
+        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors(devCorsPolicy);
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
